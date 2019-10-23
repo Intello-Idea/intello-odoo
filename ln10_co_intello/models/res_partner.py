b8c25b9490bbc2cba8dc8cb495f83212d3fa8867
Jose Daniel Corredor <jose.corredor@intelloidea.com>
2019-10-23 17:14:41 -0500

    Ajustes/Entrega: Sprint 1: Ítem 1: Ajuste campo nombre Tercero

--- ln10_co_intello/models/res_partner.py
+++ ln10_co_intello/models/res_partner.py
@@ -5,23 +5,29 @@ class Partner(models.Model):
     _inherit = 'res.partner'
 
     # Add a new column to the res.partner model, by default partners are not
-    # instructors
     name = fields.Char(index=True)
+    label_name = fields.Char(compute='_compute_label_name')
     first_name = fields.Char(default='')
     second_name = fields.Char(default='')
     surname = fields.Char(default='')
     second_surname = fields.Char(default='')
 
+    @api.depends('name')
+    def _compute_label_name(self):
+        self.label_name = self.name
+
     @api.onchange('first_name', 'second_name', 'surname', 'second_surname')
     def _compute_full_name(self):
         names = (self.first_name, self.second_name, self.surname, self.second_surname)
         full_name = ''
+
         for x in names:
-            if x != '':
+            if x:
                 if full_name != '':
-                    full_name = full_name + ' ' + x.capitalize()
+                    full_name = full_name + ' ' + x.strip().capitalize()
                 else:
-                    full_name = x.capitalize()
+                    full_name = x.strip().capitalize()
 
         self.name = full_name.strip()
+
         # self.name = ' '.join(names).strip().replace('  ', ' ')
