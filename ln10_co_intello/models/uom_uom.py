224923297a1baa96cc107c1ef17f07d3c185210e
Julián Camilo Valdés Bello <julian.valdes@intelloidea.com>
2020-09-29 13:59:10 -0500

    Ajuste

--- ln10_co_intello/models/uom_uom.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import fields, models, api, exceptions
-from odoo.tools.translate import _
-
-class UoM(models.Model):
-    _inherit = 'uom.uom'
-
-    dian_code = fields.Many2one('ln10_co_intello.diancodes', ondelete='cascade', string='DIAN Code', domain=[('type', '=', 'uom')])
--- theme_intello/models/website_menu.py
+++
@@ -1,8 +0,0 @@
-from odoo import fields, models, api
-
-
-class Menu(models.Model):
-    _inherit = "website.menu"
-
-    class_icon = fields.Char('Class icon')
-
